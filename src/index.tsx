6e252ba6f46cb9a8bee12469664b2773f3c5b5ee
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-06-08 16:17:26 +0300

    feat: add css baseline

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { CssBaseline } from '@material-ui/core';
 
 import App from './modules/App';
 
@@ -9,6 +10,8 @@ const MOUNT_NODE = document.getElementById('root');
 
 ReactDOM.render(
   <StrictMode>
+    <CssBaseline />
+
     <App />
   </StrictMode>,
   MOUNT_NODE,
